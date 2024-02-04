69e7a0915e03e00294e2b66d05212b74ad774491
skywalker512 <houzhenhong@outlook.com>
2024-02-04 16:28:50 +0800

    add js plugin

--- farm.config.ts
+++ farm.config.ts
@@ -1,8 +1,23 @@
-import { defineConfig } from '@farmfe/core';
+import { defineConfig, JsPlugin } from '@farmfe/core';
 import vue from '@vitejs/plugin-vue';
 
+const a: JsPlugin = {
+  name: 'a',
+  transform: {
+    filters: {
+      resolvedPaths: ['a.ts'],
+    },
+    executor(param, context, hookContext) {
+      console.log(param, context, hookContext);
+      return {
+        content: `import './style.scss?q=2&c=3';`,
+        moduleType: 'js',
+      };
+    },
+  },
+};
+
 export default defineConfig({
-  vitePlugins: [
-    vue(),
-  ]
+  plugins: [a],
+  vitePlugins: [vue()],
 });
